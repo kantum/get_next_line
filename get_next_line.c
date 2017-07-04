69c45fbc401bd359e4aa46cf5682a51fdc8eab69
greencrow <samuel.vigouroux.o@gmail.com>
2017-07-04 11:34:58 +0200

    ca avance

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,27 @@
 /*   By: svigouro <svigouro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/07 10:37:12 by svigouro          #+#    #+#             */
-/*   Updated: 2017/06/07 10:38:24 by svigouro         ###   ########.fr       */
+/*   Updated: 2017/07/04 11:34:41 by svigouro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "./libft/includes/libft.h"
 #include "get_next_line.h"
+
+int		ft_realloc_save(char **save)
+{
+	char	*swap;
+
+	if (!(swap = ft_strnew(ft_strlen(*save))))
+		return (0);
+	ft_strcpy(swap, *save);
+	*save = NULL;
+	if (!(*save = ft_strnew(ft_strlen(swap) + BUFF_SIZE)))
+		return (0);
+	ft_strcpy(*save, swap);
+	return (1);
+}
 
 int		get_next_line(const int fd, char **line)
 {
@@ -25,7 +39,11 @@ int		get_next_line(const int fd, char **line)
 		return (-1);
 	while ((i = read(fd, *line, BUFF_SIZE)) > 0)
 	{
-
+		if (!(ft_realloc_save(&save)))
+			return (-1);
+		ft_strncat(save, *line, BUFF_SIZE);
+		if (ft_memchr(*line, '\n', BUFF_SIZE))
+			break ;
 	}
 	return (0);
 }
