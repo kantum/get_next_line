4166f65610ccf078dc1d431491bfc53c64513b62
greencrow <samuel.vigouroux.o@gmail.com>
2017-07-13 14:37:27 +0200

    finished TODO: correct segv

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: svigouro <svigouro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/07 10:37:12 by svigouro          #+#    #+#             */
-/*   Updated: 2017/07/12 23:45:09 by svigouro         ###   ########.fr       */
+/*   Updated: 2017/07/13 14:08:58 by svigouro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,45 +14,45 @@
 #include "./libft/includes/libft.h"
 #include "get_next_line.h"
 
-int		ft_realloc_rest(char **save)
+int		ft_realloc_rest(char **rest)
 {
 	char	*swap;
 
-	if (!(swap = ft_strnew(ft_strlen(*save))))
+	if (!(swap = ft_strnew(ft_strlen(*rest))))
 		return (0);
-	ft_strcpy(swap, *save);
-	*save = NULL;
-	if (!(*save = ft_strnew(ft_strlen(swap) + BUFF_SIZE)))
+	ft_strcpy(swap, *rest);
+	*rest = NULL;
+	if (!(*rest = ft_strnew(ft_strlen(swap) + BUFF_SIZE)))
 		return (0);
-	ft_strcpy(*save, swap);
+	ft_strcpy(*rest, swap);
 	return (1);
 }
 
-int		ft_write_line(int j, char **save, char **line)
+int		ft_write_line(int j, char **rest, char **line)
 {
 	int		i;
 	int		y;
 
 	i = 0;
 	y = 0;
-	if (j != 0 || ft_strlen(*save) != 0)
+	if (j != 0 || ft_strlen(*rest) != 0)
 	{
-		while (*save[i] != '\n')
+		while (*rest[i] != '\n')
 		{
-			*line[i] = *save[i];
+			*line[i] = *rest[i];
 			i++;
 		}
 		*line[i++] = '\0';
-		if (*save[0] == '\n')
+		if (*rest[0] == '\n')
 		{
-			while (*save[i])
-				*save[y++] = *save[i++];
-			*save[y] = '\0';
+			while (*rest[i])
+				*rest[y++] = *rest[i++];
+			*rest[y] = '\0';
 			return (0);
 		}
-		while (*save[i])
-			*save[y++] = *save[i++];
-		*save[y] = '\0';
+		while (*rest[i])
+			*rest[y++] = *rest[i++];
+		*rest[y] = '\0';
 	}
 	return (1);
 }
@@ -74,5 +74,14 @@ int		get_next_line(const int fd, char **line)
 		if (ft_memchr(*line, '\n', BUFF_SIZE))
 			break ;
 	}
+	if ((ft_write_line(i, &rest, &(*line)) == 0))
+		return (1);
+	if (ft_memcmp(*line, rest, ft_strlen(*line)) == 0)
+	{
+		if (!(*line = ft_strdup("")))
+			return (1);
+		return (0);
+	}
 	return (0);
 }
+
